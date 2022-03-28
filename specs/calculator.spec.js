fd2e1b9afb37fcd1d123bb137e95cda21120511b
Barsukov <a.barsukov@rabota.ru>
2022-03-28 23:17:01 +0400

    add calculator unit test

--- specs/calculator.spec.js
+++ specs/calculator.spec.js
@@ -1,13 +1,18 @@
 import calculator from '../src/calculator';
 describe('Calculator 100% coverage', () => {
-  test('it loads without error', () => {
+  it('it loads without error', () => {
     expect(calculator).toBeDefined();
     expect(typeof calculator).toBe('function');
   });
   test.each`
     a      | op     | b      | expected
     ${2}   | ${'*'} | ${2}   | ${4}
+    ${2}   | ${'+'} | ${2}   | ${4}
+    ${2}   | ${'-'} | ${2}   | ${0}
+    ${2}   | ${'/'} | ${2}   | ${1}
     ${2}   | ${'&'} | ${2}   | ${'error'}
+    ${2}   | ${'+'} | ${'b'}   | ${'error'}
+    ${'a'}   | ${'-'} | ${2}   | ${'error'}
   `('$a $op $b = $expected', ({ a, op, b, expected }) => {
     if (expected === 'error') {
       expect(() => calculator(a, op, b)).toThrow();
